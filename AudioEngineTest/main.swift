fe42e52b620e8d9b2ff9ff6420e9a04dbb6d25b8
Christian Tietze <me@christiantietze.de>
2023-09-30 10:12:54 +0200

    pick input devices interactively

--- AudioEngineTest/main.swift
+++ AudioEngineTest/main.swift
@@ -3,6 +3,8 @@ import AppKit
 
 var exit = false
 
+var inputDevice: AVCaptureDeviceInput?
+
 func captureDevices() -> [AVCaptureDevice] {
     return AVCaptureDevice.DiscoverySession(
         deviceTypes: [.builtInMicrophone],
@@ -14,7 +16,9 @@ func captureDevices() -> [AVCaptureDevice] {
 func getInput() {
     print("> ", terminator: "")
     
-    switch readLine(strippingNewline: true) {
+    let input = readLine(strippingNewline: true)
+
+    switch input {
     case "h":
         printBanner()
 
@@ -27,10 +31,31 @@ func getInput() {
             print("No microphone devices found!")
         } else {
             devices.enumerated().forEach { (offset, device) in
+                print(offset, terminator: "\t")
                 print(device.localizedName, !device.isConnected ? "(DISCONNECTED)" : "")
             }
         }
 
+    case .some(let cmd) where cmd.hasPrefix("i"):
+        guard let inputDeviceNumber = Int(cmd.dropFirst(1))
+        else { return print("No valid device number: \(cmd)") }
+        let device = captureDevices()[inputDeviceNumber]
+        do {
+            try inputDevice = AVCaptureDeviceInput(device: device)
+            print("→ Set input device to", device.localizedName)
+        } catch {
+            print("Cannot capture input from", device.localizedName, "because", error)
+        }
+
+    case "di":
+        guard let defaultDevice = AVCaptureDevice.default(.builtInMicrophone, for: .audio, position: .unspecified) 
+        else { return print("No default input device") }
+        do {
+            try inputDevice = AVCaptureDeviceInput(device: defaultDevice)
+            print("→ Set input device to", defaultDevice.localizedName)
+        } catch {
+            print("Cannot capture input from", defaultDevice.localizedName, "because", error)
+        }
 
     default:
         NSSound.beep()
@@ -44,6 +69,8 @@ func printBanner() {
         "\th\tprint this help",
         "-----------------------------------",
         "\tls\tlist devices",
+        "\ti[0--9]\tset input device",
+        "\tdi\tset default input device",
     ].forEach { print($0) }
 }
 
