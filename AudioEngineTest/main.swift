febbb37260b9b42e3218718cd2599f6b92781cad
Christian Tietze <me@christiantietze.de>
2023-09-30 09:42:01 +0200

    print banner with commands

--- AudioEngineTest/main.swift
+++ AudioEngineTest/main.swift
@@ -7,12 +7,26 @@ func getInput() {
     print("> ", terminator: "")
     
     switch readLine(strippingNewline: true) {
-    case "q": 
-         exit = true
+    case "h":
+        printBanner()
+
+    case "q":
+        exit = true
+
     default:
         NSSound.beep()
     }
 }
+
+func printBanner() {
+    [
+        "Usage:",
+        "\tq\tquit",
+        "\th\tprint this help",
+    ].forEach { print($0) }
+}
+
+printBanner()
 
 repeat {
     getInput()
